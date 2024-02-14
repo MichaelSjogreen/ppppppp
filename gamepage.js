37b19257e5e073c0c0d840bf52a131a8f33a531e
MichaelSjogreen <147011133+MichaelSjogreen@users.noreply.github.com>
2024-02-14 18:56:44 -0500

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,7 @@
+// valor aleatorio generado
+var y = Math.floor(Math.random() * 10 + 1);
+// contar el número de intentos
+// crear para el intento correcto
+
+  
+// función para el número adivinado por el usuario     
